ac7426028305327ba7669df63f140b2105db1fdc
VladimirSharaP <zenk1998@mail.ru>
2017-02-26 18:20:25 +0700

    Реализация ввода данных

---
+++ vklad.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+int a, b;
+printf("Введите срок вклада в днях\n");
+scanf("%d", &a);
+printf("Введите сумму вклада\n");
+scanf("%d", &b);
+printf("Сумма вклада: %d\nСрок вклада: %d\n", b, a);
+}
